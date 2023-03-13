d029ecb985ab29be4b338d1b599f6e495857c620
samitizerxu <samuelzxu@gmail.com>
2023-03-12 20:58:22 -0400

    Testing modifications

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello from Render!
+            Hello from Samuel Xu!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://samuelzxu.github.io/about/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Here's my Github
           </a>
         </header>
       </div>
